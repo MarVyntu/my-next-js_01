59102e40458482a56d9d7d6d1634c8fdc7abdcdc
MarVyntu <mar.vyntu@gmail.com>
2023-03-15 17:26:35 +0100

    add my logo

--- components/header.js
+++ components/header.js
@@ -1,19 +1,27 @@
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <Image src="/logoM8.png" alt="prayer" width={50} height={50} />
-      </div>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About me</Link>
-        <Link href="/reviews">Reviews</Link>
-        <Link href="/works">My Works</Link>
-      </nav>
-    </header>
+    <>
+      <Head>
+        <title>4petals</title>
+        <link rel="shortcut icon" href="/logoM8.png" type="image/png" />
+      </Head>
+
+      <header>
+        <div>
+          <Image src="/logoM8.png" alt="prayer" width={50} height={50} />
+        </div>
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/about">About me</Link>
+          <Link href="/reviews">Reviews</Link>
+          <Link href="/works">My Works</Link>
+        </nav>
+      </header>
+    </>
   );
 };
 
